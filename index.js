06839a99f5b691a06867ef36aef39328d74fa469
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-11-20 18:32:51 -0500

    UPDATE nov 20 api txtgen

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Import the express library and assign it to a variable
 import express from 'express'
 
+// Add this line to the top of the file
+import {
+  sentence
+} from 'txtgen'
+
+// // Update the express app code below to this:
+// app.get('/', (req, res) => {
+//   const randomSentence = sentence()
+
+//   res.send(randomSentence)
+// })
+
 // Create an instance of an express application 
 const app = express()
 
@@ -9,7 +21,8 @@ const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
 app.get('/', (req, res) => {
-  res.send("soem words to display on the website")
+  res.send(sentence())
+  // res.send("hello hello! Please draw an octopus, or post an octopus related meme!! :DD")
 })
 
 // Set the application to listen a port

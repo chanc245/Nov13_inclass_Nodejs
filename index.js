fa4b129dd9e3cfb7becef71f9cdac83081e0f51c
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-11-20 19:55:02 -0500

    UPDATE cleaning code

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import { sentence, paragraph } from 'txtgen'
 import fetch from 'node-fetch'
 
 import figlet from 'figlet'
+//https://www.npmjs.com/package/figlet
 
 import { generate, count } from "random-words";
+//https://www.npmjs.com/package/random-words
 
 const app = express()
 
 const port = process.env.PORT || 3001
 
-// Set up a response for the root path of the application
 app.get('/', (req, res) => {
   // res.send(sentence())
   // res.send("hello hello! Please draw an octopus, or post an octopus related meme!! :DD")
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
     }
     const formattedData = data.replace(/ /g, '&nbsp;').replace(/\n/g, '<br>');
     res.send(`
-    a randomly generate word:[${randomword}] in ASCII
+    a randomly generate word:[${randomword}] in ASCII art form
     <br>
     <pre>${formattedData}</pre>
     `);
@@ -31,7 +32,6 @@ app.get('/', (req, res) => {
 
 })
 
-// Set the application to listen a port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

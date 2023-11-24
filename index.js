24a4f15ea28859a8e45f212469b04f7df2433b01
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-11-24 14:37:57 -0500

    UPDATE insturction

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ app.get('/', (req, res) => {
     }
     const formattedData = data.replace(/ /g, '&nbsp;').replace(/\n/g, '<br>');
     res.send(`
-    a randomly generate word:[${randomword}] in ASCII art form
+    A randomly generate word:[${randomword}] in ASCII art form
     <br>
     <pre>${formattedData}</pre>
+    <br>
+    This website is a pratice of utilizing two apis: 
+    <br>
+    1. <a href="https://www.npmjs.com/package/figlet" target="_blank">figlet</a> 
+    - turn words into ASCII Art
+    <br>
+    2. <a href="https://www.npmjs.com/package/random-words" target="_blank">random-words</a> 
+    - generate a random word
+    <br>
+    <br>
     `);
   });
 
